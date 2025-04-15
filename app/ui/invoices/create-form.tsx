69d37e0764357f8e8e5c4e2da33c6f6c2bcf7aa7
ravividhate1979 <ravindravidhate1979@gmail.com>
2025-04-15 23:52:46 +0530

    resolved the error

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -14,7 +14,7 @@ import { useActionState } from 'react';
 
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState:State = { message :null, error: {} };
+  const initialState:State = { message :null, errors: {} };
   const [state, formAction] = useActionState ( createInvoice, initialState );
   
   return (
