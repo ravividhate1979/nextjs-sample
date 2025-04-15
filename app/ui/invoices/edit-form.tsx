cac4a4ed17ad195073ff0279086b681d50366f20
ravividhate1979 <ravindravidhate1979@gmail.com>
2025-04-16 00:14:55 +0530

    corrected errors

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
 
@@ -21,7 +21,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
 
-  const initialState = { message : null, error: {} };
+  const initialState: State = { message : null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState ( updateInvoiceWithId, initialState );
   
